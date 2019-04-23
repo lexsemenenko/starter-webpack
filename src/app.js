512ed1d9db63200b3bce12f7dd2107bc63f79fec
Lex Semenenko <lex@lex.me>
2019-04-23 03:01:23 -0400

    Webpack HTML, Testing JS Bundling

--- src/app.js
+++ src/app.js
@@ -1,1 +1,6 @@
+import './first.js';
+import './second.js';
+import './third.js';
+
 const name = "Lex";
+console.log('app');
--- webpack.common.js
+++ webpack.common.js
@@ -1,10 +1,18 @@
 const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
     main: path.join(__dirname, 'src', 'app.js')
   },
   output: {
-    path: path.join(__dirname, './public')
+    path: path.join(__dirname, './public'),
+    filename: 'bundle.js'
   },
+  plugins: [
+    new HTMLWebpackPlugin({
+      filename: 'index.html',
+      template: './src/template.html'
+    })
+  ]
 }
