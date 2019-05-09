3ec0c10030706de4a628ffadad959da01b02db6d
Lex Semenenko <lex@lex.me>
2019-05-09 18:37:47 -0400

    …

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const LessLists = require('less-plugin-lists');
 
 module.exports = {
   entry: {
@@ -9,10 +11,42 @@ module.exports = {
     path: path.join(__dirname, './public'),
     filename: 'bundle.js'
   },
+
   plugins: [
     new HTMLWebpackPlugin({
       filename: 'index.html',
       template: './src/template.html'
     })
-  ]
-}
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.(le|c)ss$/, // For what files are the loaders
+        exclude: /node_modules/,
+        use: [
+          {
+            // What Loaders to use from bottom to top
+            loader: 'style-loader' // creates style nodes from JS strings
+          },
+          {
+            loader: MiniCssExtractPlugin.loader // Extracts CSS into separate files. It creates a CSS file per JS file which contains CSS
+          },
+          {
+            loader: 'css-loader' // Second, translates CSS into CommonJS
+          },
+          {
+            loader: 'postcss-loader'
+          },
+          {
+            loader: 'less-loader',
+            options: {
+              plugins: [new LessLists()],
+              strictMath: false
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,15 +1,21 @@
 const merge = require('webpack-merge');
 const path = require('path');
 const common = require('./webpack.common');
+const webpack = require('webpack');
 
 module.exports = merge(common, {
   mode: 'development',
+  devtool: 'inline-source-map',
   devServer: {
     port: process.env.PORT || 3000,
     contentBase: path.join(process.cwd(), './public'),
     watchContentBase: true,
     stats: 'none',
     quiet: false,
-    open: true
+    open: true,
+    hot: true
   },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin() // Test if it works for css with form element
+  ],
 });
